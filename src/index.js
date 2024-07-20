ce5a20d3e6af2ed591bef61bd8079be92f19de70
sumama <sumamakhan761>
2024-07-21 01:29:35 +0530

    frontend

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-rou
 import './index.css';
 
 import HomePage from './landing_page/home/HomePage';
-import Signup from './landing_page/signup/Signup';
-import Login from './landing_page/login/Login';
+
 import AboutPage from './landing_page/about/AboutPage';
 import ProductPage from './landing_page/products/ProductPage';
 import PricingPage from './landing_page/pricing/PricingPage';
@@ -13,6 +12,14 @@ import SupportPage from './landing_page/support/SupportPage';
 import Navbar from './landing_page/Navbar';
 import Footer from './landing_page/footer';
 import NotFound from './landing_page/Notfound'
+import Home from "./components/Home";
+import Funds from "./components/Funds";
+import Holdings from "./components/Holdings";
+
+// import Orders from "./components/Orders";
+// import Positions from "./components/Positions";
+// import Summary from "./components/Summary";
+// import WatchList from "./components/WatchList";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,8 +27,11 @@ root.render(
     <Navbar />
     <Routes>
       <Route path='/' element={<HomePage />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
+      <Route path="/dashboard/*" element={<Home />} />
+      {/* <Route path="/dashboard/orders" element={<Orders />} />
+      <Route path="/dashboard/holdings" element={<Holdings />} />
+      <Route path="/dashboard/positions" element={<Positions />} />
+      <Route path="/dashboard/funds" element={<Funds />} /> */}
       <Route path='/about' element={<AboutPage />} />
       <Route path='/product' element={<ProductPage />} />
       <Route path='/pricing' element={<PricingPage />} />
--- src/landing_page/Navbar.js
+++ src/landing_page/Navbar.js
@@ -17,11 +17,9 @@ function Navbar() {
 
           <form class="d-flex" role="search" style={{ marginLeft: "50%" }}>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
               <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to={"/login"}>Login</Link>
-              </li>
-              <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to={"/signup"}>Signup</Link>
+                <Link class="nav-link active" aria-current="page" to={"/dashboard"}>Dashboard</Link>
               </li>
               <li class="nav-item">
                 <Link class="nav-link active" aria-current="page" to={"/about"}>About</Link>
